b67ffb6fe1c0c4a94642bc4097ac1fa0ae2974b1
Samuel Denton <samdenton1995@hotmail.co.uk>
2020-11-13 11:01:20 +0000

    debugged my css file. Moved the css <link> tag under my JS <script> tags in the head. Working on adding and removing classes for the nav bar

--- assets/scripts/nav-bar-hide.js
+++ assets/scripts/nav-bar-hide.js
@@ -1,11 +1,31 @@
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
-  var currentScrollPos = window.pageYOffset;
+ var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("navbar").style.top = "0";
   } else {
-    document.getElementById("navbar").style.top = "-50px";
+    document.getElementById("navbar").style.top = "-65px";
   }
   prevScrollpos = currentScrollPos;
+
+  if ($(this).scrollTop() > 50) {
+    /*height in pixels when the navbar becomes non opaque*/
+    $("#navbar").addClass("dark-background");
+    $(".nav-link").removeClass("nav-item-black");
+    $(".nav-link").addClass("nav-item-white");
+  } else {
+    $("#navbar").removeClass("dark-background");
+    $(".nav-link").addClass("nav-item-black");
+     $(".nav-link").removeClass("nav-item-white");
+  }
 };
 //  ---------- Credit to w3schools.com -----------//
+
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 50) {
+    /*height in pixels when the navbar becomes non opaque*/
+    $(".opaque-navbar").addClass("opaque");
+  } else {
+    $(".opaque-navbar").removeClass("opaque");
+  }
+});
