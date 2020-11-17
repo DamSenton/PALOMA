a6ef8937237023f6e5d000e48dc807691d5508e9
Samuel Denton <samdenton1995@hotmail.co.uk>
2020-11-17 17:11:25 +0000

    fixing bugs in my drink-matcher.js file

--- assets/scripts/drink-matcher.js
+++ assets/scripts/drink-matcher.js
@@ -1,29 +1,31 @@
-   new function(drinkMatcher){
-   
-   var flavourSweet = document.getElementById("#flavour-sweet").checked
+document
+  .getElementById("#matcher-button")
+  .addEventListener("click", drinkMatcher);
+function drinkMatcher() {
+  var flavourSweet = document.getElementById("#flavour-sweet").checked;
 
-    var flavourBitter = document.getElementById("#flavour-bitter").checked
+  var flavourBitter = document.getElementById("#flavour-bitter").checked;
 
-    var flavourFruity = document.getElementById("#flavour-fruity").checked
+  var flavourFruity = document.getElementById("#flavour-fruity").checked;
 
-    var spiritRum = document.getElementById("spirit-rum").checked
+  var spiritRum = document.getElementById("spirit-rum").checked;
 
-    var spiritVodka = document.getElementById("spirit-vodka").checked
+  var spiritVodka = document.getElementById("spirit-vodka").checked;
 
-    var spiritTequila = document.getElementById("spirit-tequila").checked
+  var spiritTequila = document.getElementById("spirit-tequila").checked;
 
-    var spiritLiqueur = document.getElementById("spirit-liqueur").checked
+  var spiritLiqueur = document.getElementById("spirit-liqueur").checked;
 
-    var spiritGin = document.getElementById("spirit-gin").checked
+  var spiritGin = document.getElementById("spirit-gin").checked;
 
+  var drinkResult = document.getElementById(".drink-result");
 
-document.getElementById("#matcher-button").addEventListener("click", function(result){
-    if(".flavour-questions" == flavourSweet && ".spirit-questions" == spiritRum){
-        result = document.getElementsByClassName(".drink-result").addClass(".zombie-result")
-    }
+  if (flavourSweet == checked && spiritRum == checked) {
+    $(".drink-result").addClass(".zombie-result");
+  }
+}
 
-
-    });
-console.log(spiritGin);
-  
-}  
+// function drinkMatcher(){
+// if("#flavour-sweet".checked && "#spirit-rum".checked){
+//    document.getElementsByClassName(".drink-result").addClass(".zombie-result")
+// }
